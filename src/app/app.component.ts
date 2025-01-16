693d92e0d02025bcca29a9239e65b69df3620c91
Sergii Serdiuk <serdiuk-sergii@bustour.ua>
2025-01-16 19:57:25 +0200

    Saved work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { UserListTopComponent } from './components/user-list-top/user-list-top.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserListTopComponent],
+  imports: [HeaderComponent, UserListTopComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
